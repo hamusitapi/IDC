0d4be3e2cc6ea5c00e0e5a252db85d61f60bffce
hamusita <49346236+hamusitapi@users.noreply.github.com>
2019-07-02 18:30:01 +0800

    完成

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="zh_cn">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>个人IDC导航站</title>
+    <link rel="stylesheet" href="style.css">
+    
+</head>
+<body>
+    <header>
+        <h1>个人IDC导航站<small> - 专注个人IDC收录</small></h1>
+    </header>
+    <section id="sh">
+        <div style="display:flex;flex-wrap:wrap;justify-content:center">
+        <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "123456";
+        $dbname = "idc";
+    
+        // 创建连接
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // 检测连接
+        if ($conn->connect_error) {
+            die("数据库连接失败: " . $conn->connect_error);
+        } 
+        $sql = "SELECT ID, website_name,website_link,admin_qq,admin_mail,found_time,descriptions FROM idc";
+        $result = $conn->query($sql);
+        
+        if ($result->num_rows > 0) {
+            // 输出数据
+            while($row = $result->fetch_assoc()) {
+                echo '<div class="idc_block" title="查看详情" onclick="know('.$row["ID"].')">'.'<h3>'.$row["website_name"].'</h3>'.'<p>'. $row["descriptions"].'</p></div>';
+            }
+        } else {
+            echo "0 结果";
+        }
+        $conn->close();
+        ?>
+        </div>
+        <p>
+        
+        </p>
+    </section>
+    <footer>
+        <p><a href="">Copyright 个人IDC导航站 Rights Reserved.</a></p>
+    </footer>
+    <script>
+    function know(k) {
+        window.open("/info.php?idc="+k)
+            }
+    </script>
+</body>
+</html>
---
+++ info.php
@@ -0,0 +1,42 @@
+<?php
+   
+    $servername = "localhost";
+    $username = "root";
+    $password = "123456";
+    $dbname = "idc";
+    // 创建连接
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // 检测连接
+    if ($conn->connect_error) {
+        die("数据库连接失败: " . $conn->connect_error);
+    } 
+    $result = mysqli_query($conn,"SELECT * FROM idc WHERE ID="."'".$_GET['idc']."'");
+    
+    $row = mysqli_fetch_array($result);
+?>
+<!DOCTYPE html>
+<html lang="zh_cn">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>个人IDC导航站</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>个人IDC导航站<small> - 专注个人IDC收录</small></h1>
+    </header>
+    <section>
+        <h2><?php echo $row['website_name']; ?></h2>
+        <p>爬取时间：<?php echo $row['found_time']; ?></p>
+        <p>直达地址：<a href="<?php echo $row['website_link']; ?>" target="_blank"><?php echo $row['website_name']; ?></a></p>
+        <p><?php echo $row['descriptions']; ?></p>
+        
+    </section>
+    <?php $conn->close(); ?>
+    <footer>
+        <p><a href="">Copyright 个人IDC导航站 Rights Reserved.</a></p>
+    </footer>
+</body>
+</html>
